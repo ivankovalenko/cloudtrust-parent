e3052fd6320519e394498019e234a446fbcefbb0
lpa <lpa@elca.ch>
2022-10-31 15:26:02 +0100

    update to kc 19.0.3 + set java version to 11

--- kc-cloudtrust-module/kc-cloudtrust-common/src/main/java/io/cloudtrust/keycloak/CredentialExpiryHelper.java
+++ kc-cloudtrust-module/kc-cloudtrust-common/src/main/java/io/cloudtrust/keycloak/CredentialExpiryHelper.java
@@ -1,7 +1,7 @@
 package io.cloudtrust.keycloak;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.keycloak.credential.CredentialModel;
 import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.provider.ProviderConfigProperty;
